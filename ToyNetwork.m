6b8be8ba38a8befc198c804dc51f8c67a603e34c
Jeremie Fish <82151130+jefish003@users.noreply.github.com>
2022-02-08 10:21:22 -0500

    Update ToyNetwork.m

--- ToyNetwork.m
+++ ToyNetwork.m
@@ -1,4 +1,14 @@
 function [A] = ToyNetwork(n,gamma)
+%
+%Inputs: 
+%       n     - (pos. int.) The number of nodes
+%       gamma - (real number) The parameter for the network creation process as discussed in "Non-normality, optimality and synchronization"
+%               By Jeremie Fish and Erik Bollt
+%Outputs: A - (n x n) Adjacency matrix
+%
+%Written by: Jeremie Fish
+%Last updated February 8th 2022
+%
 
 A = zeros(n);
 A(2:end,1) = gamma;
