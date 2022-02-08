f825f4fef8599e528cfdd7c7093573e7bf19be9a
Jeremie Fish <82151130+jefish003@users.noreply.github.com>
2022-02-08 10:16:24 -0500

    Update LapPseudo.m

--- LapPseudo.m
+++ LapPseudo.m
@@ -1,5 +1,14 @@
 function [P] = LapPseudo(L,Grid)
-
+%Inputs:
+%       L    - (n x n matrix) The graph Laplacian
+%       Grid - (Optional) - The grid to calculate the Laplacian pseudospectra over, Grid should be structure as follows (an example grid)
+%                           [Z1,Z2] = ndgrid(-M:M/100:M,-M:M/100:M) and Grid.Z1 = Z1, Grid.Z2 = Z2. Obviously the ndgrid can be changed
+%                           to match whatever search region is desired.
+%
+%
+%Written by: Jeremie Fish
+%Last updated February 8th 2022
+%
 [V T] = schur(L);
 DT = diag(T);
 F = find(DT~=min(DT));
